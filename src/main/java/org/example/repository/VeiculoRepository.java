0123f4504de8878d2c4dbcf988ab1fe4d4e34e4a
Level 2D <danielcostasilva06@gmail.com>
2026-05-28 19:10:29 -0300

    Atualização de Banco de Dados

--- src/main/java/org/example/repository/VeiculoRepository.java
+++ src/main/java/org/example/repository/VeiculoRepository.java
@@ -2,6 +2,8 @@ package org.example.repository;
 
 import org.example.model.Veiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +11,6 @@ import java.util.UUID;
 
 @Repository
 public interface VeiculoRepository extends JpaRepository<Veiculo, UUID> {
-    List<Veiculo> findByOficinaId(UUID oficinaId);
+    @Query("SELECT v FROM Veiculo v WHERE v.cliente.oficina.id = :oficinaId")
+    List<Veiculo> findByOficinaId(@Param("oficinaId") UUID oficinaId);
 }
