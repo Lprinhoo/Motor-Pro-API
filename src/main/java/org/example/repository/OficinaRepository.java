68dd36308c58013860f59d9818a422058fc15992
danie <danielcostasilva06@gmail.com>
2026-06-04 09:39:09 -0300

    Atualização de Banco de Dados

--- src/main/java/org/example/repository/OficinaRepository.java
+++ src/main/java/org/example/repository/OficinaRepository.java
@@ -4,10 +4,10 @@ import org.example.model.Oficina;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional; // Import necessário
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OficinaRepository extends JpaRepository<Oficina, UUID> {
-    Optional<Oficina> findByUserUsername(String username); // Linha adicionada
+    // Optional<Oficina> findByUserUsername(String username); // Linha removida
 }
