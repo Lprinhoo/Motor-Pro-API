efec78148df5e9a42376cb098273c7ee298fc155
Level 2D <danielcostasilva06@gmail.com>
2026-06-08 15:11:35 -0300

    Atualização de Banco de Dados

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.example.config;
 import org.example.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod; // Import adicionado
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
